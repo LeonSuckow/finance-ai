2d686295079fea7335208c66496d65a8172eb9b7
Leon Suckow <leonlucassuckow@hotmail.com>
2024-11-05 22:47:41 -0300

    feat: add authentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
-import Transactions from "@/app/transactions/page";
-export default function Home() {
-  return <Transactions />;
-}
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  return (
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
+  );
+};
+
+export default Home;
