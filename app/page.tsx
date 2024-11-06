a4ceab02008f397a9855a942c4f8b3b8bdb60020
Leon Suckow <leonlucassuckow@hotmail.com>
2024-11-05 22:55:35 -0300

    chore: add git hooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-
 const Home = async () => {
   const { userId } = await auth();
   if (!userId) {
