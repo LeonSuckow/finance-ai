444c4f376c36a417414e36abd58da356391ca8c4
Leon Suckow <leonlucassuckow@hotmail.com>
2024-11-14 03:31:16 -0300

    feat: add navbar component

---
+++ app/_components/custom/navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const menuItems = {
+  Dashboard: "/",
+  Transactions: "/transactions",
+  Assinatura: "/subscription",
+};
+const Navbar = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex justify-between border-b border-solid px-8 py-4">
+      <div className="flex items-center gap-10">
+        <Image
+          src={"/logo.svg"}
+          alt="Logo finance.ai"
+          width={173}
+          height={39}
+        />
+        {Object.keys(menuItems).map((item) => (
+          <Link
+            href={menuItems[item as keyof typeof menuItems]}
+            key={item}
+            className={
+              pathname === menuItems[item as keyof typeof menuItems]
+                ? "font-bold text-primary"
+                : "text-muted-foreground"
+            }
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
+      <UserButton showName />
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,3 +1,4 @@
+import Navbar from "../_components/custom/navbar";
 import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 
@@ -7,18 +8,21 @@ import AddTransactions from "./_components/add-transaction";
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
   return (
-    <div className="space-y-6 p-6">
-      <div className="flex w-full items-center justify-between">
-        <h1 className="text-2xl font-bold">Transações</h1>
-        <AddTransactions />
-        {/* <Button className="rounded-full font-bold">
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactions />
+          {/* <Button className="rounded-full font-bold">
           Adicionar transação
           <ArrowDownUpIcon className="font-bold" />
         </Button> */}
-      </div>
+        </div>
 
-      <DataTable columns={transactionsColumns} data={transactions} />
-    </div>
+        <DataTable columns={transactionsColumns} data={transactions} />
+      </div>
+    </>
   );
 };
 
